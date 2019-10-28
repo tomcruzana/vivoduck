3317edd7d2ac7215a9c5136109a948a7ceb68f19
Tom Cruzana <tomcruzana@ymail.com>
2019-10-27 21:15:02 -0700

    Update VivoDuck_Backend.js

--- VivoDuck_Backend.js
+++ VivoDuck_Backend.js
@@ -1,23 +1,22 @@
-/*
-References:
-https://developers.google.com/apps-script/guides/html/
-https://developers.google.com/apps-script/guides/web#deploying
-//https://developers.google.com/apps-script/guides/ui-service
-https://developers.google.com/apps-script/guides/html/reference/run#withSuccessHandler(Function)
-https://stackoverflow.com/questions/16525553/get-value-of-html-text-box-in-apps-script-function
-https://www.color-hex.com/
-*/
-
-function doGet() {
-  return HtmlService.createHtmlOutputFromFile('index'); //declare as web app
+function doGet() //declare as a Google web app
+{
+  return HtmlService
+      .createTemplateFromFile('index')
+      .evaluate()
 }
 
-var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Sheet1"); //gets the name of the sheet
-var cell = sheet.getCurrentCell();
+var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Sheet1"); //get the name of the sheet
+var cell = sheet.getCurrentCell(); //get active cell
+
+function retrieveRow() //This feature is a work in progress. Function for retrieving rows
+{
+  var temp = sheet.getRange("A1:C1").getValues(); 
+  Logger.log(temp);
+  return temp;
+}
 
 function selectCell(formObject)
 {
-  //for code reuse!
   var val0 = formObject['location_txt'];
   var val1 = formObject['machineInfo_txt'];
   var val2 = formObject['vivonetPO_txt'];
@@ -30,15 +29,10 @@ function selectCell(formObject)
   var val9 = formObject['macAddress_txt'];
   var val10 = formObject['SO_txt'];
 
-
   var values = [val0, val1,val2,val3,val4,val5,val6,val7,val8,val9,val10];
-  
-  //insert for loop here if you have terminals that are alike in the same order!
   
   if (cell)
   {
-   sheet.appendRow([ values[0], values[1], values[2],values[3] , values[4],values[5] ,values[6] ,values[7] ,values[8] ,values[9] ,values[10]]);
-  //cell.setBackgroundRGB(252, 200, 12);
+   sheet.appendRow([ values[0], values[1], values[2],values[3] , values[4],values[5] ,values[6] ,values[7] ,values[8] ,values[9] ,values[10]]); //Appends the values into the cell rows.
   }
-  
 }

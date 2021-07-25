2878dec53876bb57691f459f1c7f74b001d2410b
tomcruzana <tomcruzana@ymail.com>
2021-07-25 14:04:44 -0700

    started frontend dev

---
+++ frontend/public/scripts/index.js
@@ -0,0 +1,11 @@
+const btn_1_create = document.querySelector(".btn-1")
+const btn_2_convert = document.querySelector(".btn-2")
+
+
+btn_1_create.addEventListener("click", ()=>{
+    window.location.href = "./create.html";
+})
+
+btn_2_convert.addEventListener("click", ()=>{
+    window.location.href = "./convert.html";
+})

65299138fccb6c5ad2314d9e6f5da7496c987a70
tomcruzana <tomcruzana@ymail.com>
2021-08-12 19:11:10 -0700

    added pagination, fetch pages

--- frontend/public/scripts/index.js
+++ frontend/public/scripts/index.js
@@ -1,5 +1,7 @@
 const btn_1_create = document.querySelector(".btn-1");
 const btn_2_convert = document.querySelector(".btn-2");
+const btn_search = document.querySelector("#searchBtn");
+
 
 btn_1_create.addEventListener("click", () => {
   window.location.href = "./create.html";
@@ -7,4 +9,8 @@ btn_1_create.addEventListener("click", () => {
 
 btn_2_convert.addEventListener("click", () => {
   window.location.href = "./convert.html";
+});
+
+btn_search.addEventListener("click", () => {
+  window.location.href = "./fetch-results.html";
 });
